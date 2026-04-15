020f1048ebf50cf312ccdbb4b107573010d262c4
NouDaimon <peric.iason@gmail.com>
2026-04-15 17:56:09 +0300

    feat: support both CJS + ESM

--- sdk/tsup.config.ts
+++ sdk/tsup.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
+  outExtension({ format }) {
+    return format === 'cjs' ? { js: '.cjs', dts: '.d.cts' } : { js: '.js', dts: '.d.ts' }
+  },
   clean: true,
   sourcemap: true,
   treeshake: true,
